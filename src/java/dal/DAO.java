0cf77bceb0caa3c06912eb08608d700c667ac711
Lenovo <you@example.com>
2022-09-26 21:22:59 +0700

    Merge origin/Tien'sBranch into Tien'sBranch

--- src/java/dal/DAO.java
+++ src/java/dal/DAO.java
@@ -18,38 +18,38 @@ import model.*;
  */
 public class DAO {
 
-    private ArrayList<User> usList;
-    private ArrayList<UserProfile> usProList;
+    private ArrayList<Users> usList;
+    private ArrayList<UserProfiles> usProList;
     private String status;
     private Connection con;
 
     public DAO() {
     }
 
-    public ArrayList<User> getUsList() {
+    public ArrayList<Users> getUsList() {
         return usList;
     }
 
-    public ArrayList<UserProfile> getUsProList() {
+    public ArrayList<UserProfiles> getUsProList() {
         return usProList;
     }
 
-    public void setUsList(ArrayList<User> usList) {
+    public void setUsList(ArrayList<Users> usList) {
         this.usList = usList;
     }
 
-    public void setUsProList(ArrayList<UserProfile> usProList) {
+    public void setUsProList(ArrayList<UserProfiles> usProList) {
         this.usProList = usProList;
     }
 
-    public ArrayList<User> getUserList() {
-        ArrayList<User> uList = new ArrayList<>();
+    public ArrayList<Users> getUserList() {
+        ArrayList<Users> uList = new ArrayList<>();
         String sql = "Select * from User";
         try {
             PreparedStatement ps = con.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
-                uList.add(new User(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getString(4), rs.getBoolean(5)));
+                uList.add(new Users(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getString(4), rs.getBoolean(5)));
             }
         } catch (SQLException e) {
             status = "Error at read User" + e.getMessage();
@@ -57,14 +57,14 @@ public class DAO {
         return uList;
     }
 
-    public ArrayList<UserProfile> getUserProList() {
-        ArrayList<UserProfile> uProList = new ArrayList<>();
+    public ArrayList<UserProfiles> getUserProList() {
+        ArrayList<UserProfiles> uProList = new ArrayList<>();
         String sql = "Select * from User";
         try {
             PreparedStatement ps = con.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
-                uProList.add(new UserProfile(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getDate(6).toLocalDate(), rs.getInt(7), rs.getBoolean(8), rs.getDate(9).toLocalDate()));
+                uProList.add(new UserProfiles(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getDate(6).toLocalDate(), rs.getInt(7), rs.getBoolean(8), rs.getDate(9).toLocalDate()));
             }
         } catch (SQLException e) {
             status = "Error at read User_Profile" + e.getMessage();
