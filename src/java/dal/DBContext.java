82274b8a253b7805e90b272e855d2b256be97b62
Lenovo <you@example.com>
2022-09-26 10:10:11 +0700

    my initial commit

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -23,8 +23,8 @@ public class DBContext {
     {
         try {
             String user = "sa";
-            String pass = "ngoc1280";
-            String url = "jdbc:sqlserver://localhost:1280;databaseName=PRJ301_SE1625";
+            String pass = "123456";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=Happy_Programming";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
         } catch (ClassNotFoundException ex) {
