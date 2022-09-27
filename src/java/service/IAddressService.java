5a876e043c40a6ac974c842c404a6527b7bc5b30
sabzabi <113032368+sabzabi@users.noreply.github.com>
2022-09-27 08:19:17 +0700

    commit

---
+++ src/java/service/IAddressService.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package service;
+
+import java.util.List;
+import model.Address;
+
+/**
+ *
+ * @author minhd
+ */
+public interface IAddressService {
+    Address getAddressById(int id, List<Address> list);
+    String insert(Address u,List<Address> list);
+
+    String update(Address u,List<Address> list);
+
+    String delete(Address u,List<Address> list);
+}
